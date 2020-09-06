f198f92cfff48d76b011b7c3007a0f6a4d267c17
heech912 <34178802+heech912@users.noreply.github.com>
2020-09-06 19:43:25 +0900

    HCL E03

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            fontFamily: "Montserrat",
+            fontWeight : 700,
+            margin: "50px 0px 0px 6px",
+            fontSize: 100,
+          }}
+        >
+          Lorem
+        </div>
+        <div
+          style={{
+            fontFamily: "Montserrat",
+            fontWeight : 700,
+            margin: "90px 0px 0px 10px",
+            fontSize: 100
+          }}
+        >
+          ipsum
+        </div>
+        <img src ={"https://wallpapersite.com/images/pages/pic_w/18334.jpg"} style = {{margin: "100px 0px 0px 10px"}} />
+        <div
+          style={{
+            fontFamily: "Montserrat",
+            fontWeight : 600,
+            margin: "80px 0px 0px 6px",
+            fontSize: 50,
+          }}
+        >
+          Lorem ipsum
+        </div>
+        <p style={{
+          fontFamily: "Montserrat",
+          fontWeight : 200,
+          margin: "40px 0px 0px 6px",
+          fontSize: 14,
+          width : 1300
+        }}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet,
+consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi  </p>
+      </div>
+    );
+  }
+}
+
+export default App;
